2168bd825bd56dac3f10532f1f40a838c97095dc
NomoreiMe <eldomatt2017@gmail.com>
2024-03-21 17:41:39 +0530

    Complete Features section

--- js/script.js
+++ js/script.js
@@ -10,3 +10,31 @@ hamburgerBtn.addEventListener('click', () => {
 
 	logo.classList.toggle('fill-white');
 });
+
+// Features Sections: Tabs
+
+const tabs = Array.from(document.getElementsByClassName('tab'));
+const features = Array.from(document.getElementsByClassName('feature'));
+
+tabs.forEach((el) => {
+	const tabNumber = `tab-${tabs.indexOf(el) + 1}`;
+	const currentTab = document.querySelector(`.${tabNumber}`);
+	const featureNumber = `feature-${tabs.indexOf(el) + 1}`;
+	const currentFeature = document.querySelector(`.${featureNumber}`);
+
+	currentTab.addEventListener('click', () => {
+		tabs.forEach((el) => {
+			el.classList.remove('active-tab');
+		});
+
+		features.forEach((el) => {
+			el.classList.remove('hidden');
+
+			if (el !== currentFeature) {
+				el.classList.add('hidden');
+			}
+		});
+
+		currentTab.classList.add('active-tab');
+	});
+});
